c92b0cfea9a54c6cd86ed7c02cc07c9e8af72dc4
Esubalew <esuwo2024@gmail.com>
2025-07-30 05:20:41 +0300

    Final commit for MYSQL

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
-const mysql = require("mysql2/promise");
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-const db = mysql.createPool({
+dotenv.config();
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-module.exports = db;
+export default pool;
